fad34212a7869945834389720b2fc17a868c5b59
faultbased <s_cammorto@outlook.com>
2022-02-09 15:16:52 -0500

    index code cleanup/formatted

--- server/index.js
+++ server/index.js
@@ -1,47 +1,43 @@
-const express = require("express");
+const express = require('express');
 
-//calls express library and runs it
+// calls express library and runs it
 const app = express();
 
-//calls cors middleware
-const cors = require("cors");
-const pool = require("./db");
+// calls cors middleware
+const cors = require('cors');
+const pool = require('./db');
 
-//middleware
+// middleware
 app.use(cors());
-app.use(express.json()); //req. body
+app.use(express.json()); // req. body
 
-/*in a full-stack application,
-we need to get data from the client-side; which 
+/* in a full-stack application,
+we need to get data from the client-side; which
 is taken from a request.body object; request json
-body in this case*/
+body in this case */
 
-//routes//
+// routes//
 
-//create a todo
-app.post("/todos", async (req, res) => {
-	try {
-		const { description } = req.body;
-		const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]
-		);
+// create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [description]);
 
-		res.json(newTodo);
-	} catch (err) {
-		console.error(err.message);
-	}
-
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
-//get all todos
+// get all todos
 
-//update a todo
+// update a todo
 
-//delete a todo
+// delete a todo
 
-
-//in order to create server/to jumpstart it
+// in order to create server/to jumpstart it
 app.listen(5000, () => {
-
-//delivers msgs up confirmation of server connection
-	console.log("Server is up and running on port 5000");
+// delivers msgs up confirmation of server connection
+  console.log('Server is up and running on port 5000');
 });
