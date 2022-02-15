3dcd7546c2825720a7f27bf4b70b1a96e5af98ba
faultbased <s_cammorto@outlook.com>
2022-02-15 12:32:50 -0500

    updated routes are incomplete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 // calls express library and runs it
 const app = express();
 
 // calls cors middleware
-const cors = require('cors');
-const pool = require('./db');
+const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
@@ -19,10 +19,13 @@ body in this case */
 // routes//
 
 // create a todo
-app.post('/todos', async (req, res) => {
+app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
-    const newTodo = await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [description]);
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
 
     res.json(newTodo);
   } catch (err) {
@@ -32,12 +35,51 @@ app.post('/todos', async (req, res) => {
 
 // get all todos
 
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// get a todo
+
+app.get("/todos/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE uid = $1", [uid]);
+    res.json(todo.rows[0]);
+    // calls upon uid variable
+    console.log(req.params);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // update a todo
+
+/*
+app.put("/todos/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE uid = $2",
+      [description, uid]
+    );
+    res.json("Todo was successfully updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+*/
 
 // delete a todo
 
 // in order to create server/to jumpstart it
 app.listen(5000, () => {
-// delivers msgs up confirmation of server connection
-  console.log('Server is up and running on port 5000');
+  // delivers msgs up confirmation of server connection
+  console.log("Server is up and running on port 5000");
 });
